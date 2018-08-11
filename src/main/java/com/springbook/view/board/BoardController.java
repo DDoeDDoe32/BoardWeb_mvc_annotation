4fe2a9160d935314a7e56a1f6c659af39b65e9f4
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2018-08-11 15:43:13 +0900

    MVC Annotation Quick Spring using MySQL

---
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -0,0 +1,113 @@
+package com.springbook.view.board;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springbook.biz.board.BoardListVO;
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+
+@Controller
+@SessionAttributes("board")
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	// 글 등록
+	@RequestMapping(value="/insertBoard.do")
+	public String insertBoard(BoardVO vo) throws IOException {
+		// 파일 업로드 처리
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("C:/workspace/" + fileName));
+		}
+		
+		boardService.insertBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 수정
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.updateBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 삭제
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 상세 조회
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); // Model 정보 저장
+		/*mav.addObject("board", boardDAO.getBoard(vo));	// Model 정보 저장 (mav -> String return type 변환)
+		mav.setViewName("getBoard.jsp");*/
+		return "getBoard.jsp";
+	}
+	
+	// 글 목록 검색
+	@RequestMapping("/getBoardList.do")
+	public String getBoardList(/*@RequestParam(value="searchCondition", defaultValue="TITLE", required=false)
+			String condition, @RequestParam(value="searchKeyword", defaultValue="", required=false)
+			String keyword,*/ BoardVO vo, Model model) {
+		/*System.out.println("검색 조건 : " + condition);
+		System.out.println("검색 단어 : " + keyword);*/
+		
+		// Null Check
+		if(vo.getSearchCondition() == null) vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword() == null) vo.setSearchKeyword("");
+		
+		// Model 정보 저장
+		model.addAttribute("boardList", boardService.getBoardList(vo)); // Model 정보 저장
+		/*mav.addObject("boardList", boardDAO.getBoardList(vo));	// Model 정보 저장 (mav -> String return type 변환)
+		mav.setViewName("getBoardList.jsp");*/
+		return "getBoardList.jsp";
+	}
+	
+	//검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
+	
+	// Json으로 변환
+	/*@RequestMapping("/dataTransform.do")
+	@ResponseBody
+	public List<BoardVO> dataTransform(BoardVO vo) {
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		return boardList;
+	}*/
+	
+	// Xml로 변환
+	@RequestMapping("/dataTransform.do")
+	@ResponseBody
+	public BoardListVO dataTransform(BoardVO vo) {
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		BoardListVO boardListVO = new BoardListVO();
+		boardListVO.setBoardList(boardList);
+		return boardListVO;
+	}
+}
